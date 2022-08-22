bb314d4ba40f2e7637c8311a9c23e2f65c763b16
Yonatan Bitton <bityob@gmail.com>
2022-08-22 09:21:09 +0300

    Use global storage for all tweets and hide them instead of low opacity

--- contentScript.js
+++ contentScript.js
@@ -14,7 +14,7 @@ Date.prototype.yyyymmdd = function () {
 };
 
 // load all the tweets that has been seen so far that day
-let today = new Date().yyyymmdd();
+let today = "global"; //new Date().yyyymmdd();
 chrome.storage.local.get(today, function (store) {
   if (!!store[today]) {
     seenTweets = JSON.parse(store[today]);
@@ -36,7 +36,7 @@ function listTweetIds() {
         continue;
       }
       if (seenTweets.indexOf(matches.groups.tweetId) !== -1) {
-        tweet.setAttribute("style", "opacity: 50%;");
+        tweet.setAttribute("style", "display: none;");
       } else if (unSeenTweets.indexOf(matches.groups.tweetId) === -1) {
         unSeenTweets.push(matches.groups.tweetId);
       }
@@ -53,23 +53,23 @@ function listTweetIds() {
  * Show a small indicator to denote that Just Arrived is loaded into the page
  */
 
-function showIndicator() {
-  const container = document.createElement("div")
-  container.setAttribute("id", "just-arrived-notifier")
-  container.setAttribute("style", "position: fixed; top: 10px; right: 10px; padding: 0.5rem; border: 1px solid #29B6F6; background-color: #E1F5FE; z-index: 100; font-size: xx-small;")
-  const text = document.createTextNode("Just Arrived")
-  container.appendChild(text);
-  let body = document.querySelector("body");
-  body.appendChild(container);
-  setTimeout(removeIndicator, 15000);
-}
+// function showIndicator() {
+//   const container = document.createElement("div")
+//   container.setAttribute("id", "just-arrived-notifier")
+//   container.setAttribute("style", "position: fixed; top: 10px; right: 10px; padding: 0.5rem; border: 1px solid #29B6F6; background-color: #E1F5FE; z-index: 100; font-size: xx-small;")
+//   const text = document.createTextNode("Just Arrived")
+//   container.appendChild(text);
+//   let body = document.querySelector("body");
+//   body.appendChild(container);
+//   setTimeout(removeIndicator, 15000);
+// }
 
-function removeIndicator() {
-  let body = document.querySelector("body");
-  body.querySelector("#just-arrived-notifier").remove();
-}
+// function removeIndicator() {
+//   let body = document.querySelector("body");
+//   body.querySelector("#just-arrived-notifier").remove();
+// }
 
 
 let timer = setInterval(listTweetIds, 2000);
-showIndicator();
+// showIndicator();
 
